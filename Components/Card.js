36d7ca25ed39bddc62057aeeb98ca8d2406839da
Julian Miller <73328279+docjulz@users.noreply.github.com>
2022-05-10 09:42:00 -0700

    completed wireframe
    completed basic layout.  need to clean code and adjust small details

--- Components/Card.js
+++ Components/Card.js
@@ -1,19 +1,23 @@
 import React from "react"
 
 export default function Card(props) {
+    let cardBadge
+    if (cardBadge === "yes") {
+        cardBadge = "Free Parking";
+    }
     return (
-        <div className="card__container">
+        <div className="card__container card__divider">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
             <img src={props.location.imageUrl} className="card__img" />
-            <div className="card__header">
-                <span><i class="fa-solid fa-location-dot"></i>icon</span>
-                <span>{props.location.location}</span>
-                <span><a href={props.location.googleMapsUrl}>View on Google Maps</a></span>
+            <div className="card__content">
+                <span className="card__icon"><i class="fa-solid fa-location-dot" ></i></span>
+                <span className="card__location">{props.location.location} </span>
+                <span ><a href={props.location.googleMapsUrl} className="card__map">View on Google Maps</a></span>
+                <h2 className="card__title">{props.location.title}</h2>
+                <p className="card__dates">{props.location.startDate}-{props.location.endDate}</p>
+                <p className="card__body">{props.location.description}</p>
             </div>
-            <div className="card__body">
-                <p>title</p>
-                <p>dates</p>
-                <p>body</p>
-            </div>
+            
         </div>
     )
 }
--- data.js
+++ data.js
@@ -7,7 +7,9 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        freeParking: no,
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        
     },
     {
         id: 2,
@@ -16,8 +18,9 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/QV8zmGA6ze1Qe3QZ6",
         startDate: "May 9, 2022",
         endDate: "May 12, 2022",
-        description: "A steep hike from a parking lot leads to this sandy beach offering surfing & swimming.",
-        imageUrl: "https://unsplash.com/photos/J8dGw1ogfN0"
+        description: "Salt Creek Beach Park is a popular surfing location. A small, offshore reef creates some of the best left swells along the entire coastline. Swimming, body surfing, sunbathing and tidepool exploration (all tidepool inhabitants are protected) are other beach activities.",
+        freeParking: no,
+        imageUrl: "https://source.unsplash.com/J8dGw1ogfN0"
     },
     {
         id: 3,
@@ -27,6 +30,7 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        freeParking: no,
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
     },
 ]
